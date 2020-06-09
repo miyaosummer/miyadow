e27ea694574110a92317c673edd5dc06cffb1e62
miyaosummer <yoshihikon9@gmail.com>
2020-06-09 19:52:30 +0900

    add index-action
    インデックスアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"pages#index"
-  resources :posts, only: [:new ,:create]
+  resources :posts, only: [:new ,:create,:index]
 end

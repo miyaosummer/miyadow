4bb4f7b15f8d674575a8071f0d98afe2a4b3bea5
miyaosummer <yoshihikon9@gmail.com>
2020-06-12 20:56:19 +0900

    add tests-index
    testコントローラーのindexアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:"posts#index"
+  root to:"tests#index"
   resources :posts, only: [:new ,:create,:index,:show,:destroy]
+  resources :tests
 end
